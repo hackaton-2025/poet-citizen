02146ff552b8078aa833ab22e6ec195524861f68
nikerfe <70719884+nikerfe@users.noreply.github.com>
2020-12-22 23:16:58 +0300

    styles:navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 const Navigation = ({ url }) => {
   return (
     <nav className="navigation">
+      <div className="navigation__header">
+        <h2 className="navigation__title">Мой аккаунт</h2>
+      </div>
       <ul className="navigation__item-list">
         <li className="navigation__item">
           <Link className="navigation__item-link" to={`${url}/calls`}>
@@ -24,7 +27,7 @@ const Navigation = ({ url }) => {
         </li>
         <li className="navigation__item">
           <Link className="navigation__item-link" to="/about">
-            <div className="navigation__item-image navigation__item-image_type_profile"></div>
+            <div className="navigation__item-image navigation__item-image_type_information"></div>
             <p className="navigation__item-title">О проекте</p>
           </Link>
         </li>
