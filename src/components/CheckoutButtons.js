c28ddbebc6903a06e6743db2cc2375cb464addab
Julia Doroshina <up.doroshina@yandex.ru>
2020-12-25 21:08:48 +0300

    feat: extract CheckoutBtns & add prevBtn onClick

---
+++ src/components/CheckoutButtons.js
@@ -0,0 +1,47 @@
+import cn from 'classnames';
+import prevIcon from '../images/prev-icon.svg';
+import NextIcon from '../images/next-icon.svg';
+
+const CheckoutButtons = ({
+  isNextButtonActive,
+  isPrevButton,
+  onPrevButtonClick,
+  onNextButtonClick,
+  nextButtonType = 'button',
+}) => {
+  const nextButtonClassName = cn(
+    'new-call__next-button',
+    {'new-call__next-button_active': isNextButtonActive},
+    {'new-call__next-button_disabled': !isNextButtonActive},
+  );
+
+
+  // Сделать main__content грид-контейнером, расположить элементы( заголовок, пагинацию, карточки, кнопку назад, кнопку вперед в грид-областях)
+  // те области, которые скрываются (кнопки, пагинация) -- min-content
+  // Стили для кнопки вперед: для неактивной new-call__next-button_disabled, для активной new-call__next-button_active, общие new-call__next-button
+  
+  return (
+    <>
+      { isPrevButton && (
+        <button
+          type="button"
+          className="new-call__prev-button"
+          onClick={onPrevButtonClick}
+        >
+          <img src={prevIcon} alt="Стрелка назад" /> Назад
+        </button>
+      )}
+      <button
+        type="button"
+        className={nextButtonClassName}
+        onClick={onNextButtonClick}
+        disabled={!isNextButtonActive}
+      >
+        { nextButtonType === "button" && <>{"Далее"} <img src={NextIcon} alt="Стрелка вперёд" /></> }
+        { nextButtonType === "submit" && "Отправить" }
+      </button>
+    </>
+  );
+};
+
+export default CheckoutButtons;
