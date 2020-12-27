22d09d3a86a8a37e8c6ae495a246e573cafebca2
Julia Doroshina <up.doroshina@yandex.ru>
2020-12-27 17:57:48 +0300

    feat: extract FormInput & refactor Login

---
+++ src/components/FormInput.js
@@ -0,0 +1,45 @@
+import cn from 'classnames';
+
+const FormInput = ({
+  name,
+  labelText,
+  value,
+  error,
+  type,
+  minLength,
+  maxLength,
+  pattern,
+  required,
+  onChange,
+}) => {
+
+  const inputClassName = cn(
+    'form__input',
+    {
+      'form__input_valid': error === '',
+      'form__input_invalid': error,
+      'form__input_on-load': !error,
+    },
+  );
+
+  return (
+    <div className="form__input-container">
+      <label className="form__label" htmlFor="email">{labelText}</label>
+      <input
+        id={name}
+        name={name}
+        type={type}
+        pattern={pattern}
+        minLength={minLength}
+        maxLength={maxLength}
+        required={required}
+        value={value}
+        onChange={onChange}
+        className={inputClassName}
+      />
+      <span className="form__input-error">{error && error}</span>
+    </div>
+  );
+};
+
+export default FormInput;
